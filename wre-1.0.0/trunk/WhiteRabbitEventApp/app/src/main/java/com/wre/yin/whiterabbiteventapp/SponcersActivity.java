3a20022db75c67ab4c077e89e97c240404ae2acb
prasad <prasad@49445b27-e4aa-4b11-8558-880386277ef2>
2016-05-11 16:47:26 +0000

    commit

--- wre-1.0.0/trunk/WhiteRabbitEventApp/app/src/main/java/com/wre/yin/whiterabbiteventapp/SponcersActivity.java
+++ wre-1.0.0/trunk/WhiteRabbitEventApp/app/src/main/java/com/wre/yin/whiterabbiteventapp/SponcersActivity.java
@@ -1,13 +1,27 @@
 package com.wre.yin.whiterabbiteventapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.wre.yin.whiterabbiteventapp.adapters.CheeseDynamicAdapter;
+import com.wre.yin.whiterabbiteventapp.beans.Cheeses;
+import com.wre.yin.whiterabbiteventapp.gridlibrary.DynamicGridView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SponcersActivity extends AppCompatActivity {
 
     private TextView text;
+    private DynamicGridView gridView;
+    private static final String TAG = SponcersActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +29,59 @@ public class SponcersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sponcers);
 
         String nameTxt = getIntent().getExtras().getString("name");
-        text = (TextView) findViewById(R.id.activity_text);
+        //  text = (TextView) findViewById(R.id.activity_text);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(nameTxt);
 
 
-        text.setText(nameTxt);
+        // text.setText(nameTxt);
+        //
+        gridView = (DynamicGridView) findViewById(R.id.dynamic_grid_sponcers);
 
+        gridView.setAdapter(new CheeseDynamicAdapter(this,
+                new ArrayList<String>(Arrays.asList(Cheeses.sCheeseStrings)),
+                getResources().getInteger(R.integer.column_count)));
+
+        gridView.setOnDragListener(new DynamicGridView.OnDragListener() {
+            @Override
+            public void onDragStarted(int position) {
+                Log.d(TAG, "drag started at position " + position);
+            }
+
+            @Override
+            public void onDragPositionsChanged(int oldPosition, int newPosition) {
+                Log.d(TAG, String.format("drag item position changed from %d to %d", oldPosition, newPosition));
+            }
+        });
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                gridView.startEditMode(position);
+                return true;
+            }
+        });
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(SponcersActivity.this, parent.getAdapter().getItem(position).toString(),
+                        Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(SponcersActivity.this, ProfileDetailsActivity.class);
+                i.putExtra("speakerName", parent.getAdapter().getItem(position).toString());
+                startActivity(i);
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (gridView.isEditMode()) {
+            gridView.stopEditMode();
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
