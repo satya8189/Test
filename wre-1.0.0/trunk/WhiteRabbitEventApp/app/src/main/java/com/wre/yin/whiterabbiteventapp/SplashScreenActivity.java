b38c07c1fc6f5df2df9b5b8922b4956c4036c78c
prasad <prasad@49445b27-e4aa-4b11-8558-880386277ef2>
2016-06-08 10:29:28 +0000

    commit

--- wre-1.0.0/trunk/WhiteRabbitEventApp/app/src/main/java/com/wre/yin/whiterabbiteventapp/SplashScreenActivity.java
+++ wre-1.0.0/trunk/WhiteRabbitEventApp/app/src/main/java/com/wre/yin/whiterabbiteventapp/SplashScreenActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -13,11 +11,9 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
+        getSupportActionBar().hide();
 
         new Handler().postDelayed(new Runnable() {
 
