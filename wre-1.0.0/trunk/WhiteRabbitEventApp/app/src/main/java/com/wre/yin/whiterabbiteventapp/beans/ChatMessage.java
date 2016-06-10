8a4ddbc45b9237dce80a4fafb658e2cd30654d86
anil <anil@49445b27-e4aa-4b11-8558-880386277ef2>
2016-06-10 09:47:56 +0000

    commot

--- wre-1.0.0/trunk/WhiteRabbitEventApp/app/src/main/java/com/wre/yin/whiterabbiteventapp/beans/ChatMessage.java
+++ wre-1.0.0/trunk/WhiteRabbitEventApp/app/src/main/java/com/wre/yin/whiterabbiteventapp/beans/ChatMessage.java
@@ -1,13 +1,31 @@
 package com.wre.yin.whiterabbiteventapp.beans;
 
-public class ChatMessage {
+public class  ChatMessage {
     private boolean isImage, isMine;
-    private String content;
+    private String content,name,time;
 
-    public ChatMessage(String message, boolean mine, boolean image) {
+    public ChatMessage(String message, boolean mine, boolean image,String cName,String cTime) {
         content = message;
         isMine = mine;
         isImage = image;
+        name=cName;
+        time=cTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 
     public String getContent() {
